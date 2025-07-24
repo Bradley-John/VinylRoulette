e138486f103c908fcf66827086d16c30dc88c5fa
John Bradley <j.r.bradley014@gmail.com>
2025-07-24 11:44:38 -0400

    small comment

--- src/backend/app/routes.py
+++ src/backend/app/routes.py
@@ -6,5 +6,5 @@ api = Blueprint('api', __name__)
 @api.route('/spin', methods=['GET'])
 def spin():
     genre = "Rock" #request.args.get('genre')
-    album = get_random_album(genre=genre)
+    album = get_random_album(genre=genre) #record
     return jsonify(album)
